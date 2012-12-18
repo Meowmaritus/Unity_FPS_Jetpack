f5dc24ab5ca8f4a0ea0c2a08cd23c80c1140e0d9
aexyl93 <apathetic.inanity@gmail.com>
2012-12-18 15:07:34 -0600

    small update
    added preview box for boxbuilder, cleaned up missing scripst from
    project.

--- Assets/Scripts/JS/ClickHandler.js
+++ Assets/Scripts/JS/ClickHandler.js
@@ -1,6 +1,7 @@
 #pragma strict
 
 var myVoxel : GameObject;
+var previewBox : GameObject;
 var locs : Hashtable;
 var symmetryOn : boolean;
 var symLoc : float;
@@ -19,47 +20,50 @@ function Update () {
 	if (Input.GetKeyDown(KeyCode.Escape))
 		Application.Quit();
 
-	if (Input.GetMouseButtonUp(0)) {
-		var middle = Vector3(Screen.width/2, Screen.height/2, 0);
-		var ray : Ray = Camera.mainCamera.ScreenPointToRay(middle);//Input.mousePosition);
-		var hit : RaycastHit;
-		if (Physics.Raycast(ray, hit)) {
-			//var ds : String;
-			var loc : Vector3 = hit.point;
-			//ds = "RayHit: " + hit.point;
-			var locInt = Vector3(Mathf.RoundToInt(loc.x), Mathf.RoundToInt(loc.y), Mathf.RoundToInt(loc.z));
-			//ds += " -- Before: " + locInt;
-			if (locs.ContainsKey(locInt)) {
-				if (Mathf.Approximately(Mathf.Abs(loc.x - locInt.x), 0.5))
-					locInt.x += Mathf.RoundToInt(1.1*(loc.x - locInt.x));
-				if (Mathf.Approximately(Mathf.Abs(loc.y - locInt.y), 0.5))
-					locInt.y += Mathf.RoundToInt(1.1*(loc.y - locInt.y));
-				if (Mathf.Approximately(Mathf.Abs(loc.z - locInt.z), 0.5))
-					locInt.z += Mathf.RoundToInt(1.1*(loc.z - locInt.z));
-				//ds += " -- After: " + locInt;
-			}
-			else
-				locs.Add(locInt, 0);
-			//Debug.Log(ds);
-			Instantiate(myVoxel, locInt, transform.rotation);
-			if (symmetryOn) {
-				if (symX) {
-					locInt.x = -locInt.x;
-					Instantiate(myVoxel, locInt, transform.rotation);
-					locs.Add(locInt, 0);
-				}
-				if (symY) {
-					locInt.y = -locInt.y;
-					Instantiate(myVoxel, locInt, transform.rotation);
-					locs.Add(locInt, 0);
-				}
-				if (symZ) {
-					locInt.z = -locInt.z;
-					Instantiate(myVoxel, locInt, transform.rotation);
-					locs.Add(locInt, 0);
-				}
-			}
+	
+	var middle = Vector3(Screen.width/2, Screen.height/2, 0);
+	var ray : Ray = Camera.mainCamera.ScreenPointToRay(middle);//Input.mousePosition);
+	var hit : RaycastHit;
+	if (Physics.Raycast(ray, hit)) {
+		var loc : Vector3 = hit.point;
+		var locInt = Vector3(Mathf.RoundToInt(loc.x), Mathf.RoundToInt(loc.y), Mathf.RoundToInt(loc.z));
+		
+		if (locs.ContainsKey(locInt)) {
+			if (Mathf.Approximately(Mathf.Abs(loc.x - locInt.x), 0.5))
+				locInt.x += Mathf.RoundToInt(1.1*(loc.x - locInt.x));
+			if (Mathf.Approximately(Mathf.Abs(loc.y - locInt.y), 0.5))
+				locInt.y += Mathf.RoundToInt(1.1*(loc.y - locInt.y));
+			if (Mathf.Approximately(Mathf.Abs(loc.z - locInt.z), 0.5))
+				locInt.z += Mathf.RoundToInt(1.1*(loc.z - locInt.z));
 		}
+		if (Input.GetMouseButtonUp(0)) {
+			var v : GameObject = Instantiate(myVoxel, locInt, transform.rotation);
+			locs.Add(locInt, v);
+		}
+		else {
+			previewBox.transform.position = locInt;
+			previewBox.renderer.enabled = true;
+		}
+		/*if (symmetryOn) {
+			if (symX) {
+				locInt.x = -locInt.x;
+				Instantiate(myVoxel, locInt, transform.rotation);
+				locs.Add(locInt, 0);
+			}
+			if (symY) {
+				locInt.y = -locInt.y;
+				Instantiate(myVoxel, locInt, transform.rotation);
+				locs.Add(locInt, 0);
+			}
+			if (symZ) {
+				locInt.z = -locInt.z;
+				Instantiate(myVoxel, locInt, transform.rotation);
+				locs.Add(locInt, 0);
+			}
+		}*/
+	}
+	else {
+		previewBox.renderer.enabled = false;
 	}
 	
 	if (Input.GetKeyUp("x"))
